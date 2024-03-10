4a34561a7e2df0f8348e65818c8acafe6f33ce1e
MuizZatam <muizzatam110@gmail.com>
2024-03-10 14:26:33 +0530

    Implement asr (automatic speech recognition)
    Using [vosk](alphacephei.com/vosk/) and [pyaudio](pypi.org/project/PyAudio/),
    asr is built on the Accurate generic US English model.
    This commit is just a generic build and will be modularized in later commits.

---
+++ AUDIO-MODULES/asr.py
@@ -0,0 +1,19 @@
+from vosk import Model, KaldiRecognizer
+import pyaudio
+
+model = Model('./vosk-model-en-us-0.22-lgraph')
+recognizer = KaldiRecognizer(model, 16000)
+
+capture = pyaudio.PyAudio()
+stream = capture.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+
+while True:
+
+    data = stream.read(4096, exception_on_overflow=False)
+
+    if len(data) == 0:
+        break
+
+    if recognizer.AcceptWaveform(data):
+        print(recognizer.Result())
