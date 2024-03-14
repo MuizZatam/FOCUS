c38f5b5ccd289eb2dadd975749608e8b929c60f8
MuizZatam <muizzatam110@gmail.com>
2024-03-14 13:03:29 +0530

    Update ASR to JS webkit API
    Built the basic framework and connected with
    main.py using eel framework.

--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-from vosk import Model, KaldiRecognizer
-import pyaudio
-import pyttsx3
+import eel
 
-def model():
-
-    model = Model('./vosk-model-en-us-0.22-lgraph')
-    recognizer = KaldiRecognizer(model, 16000)
-
-    capture = pyaudio.PyAudio()
-    stream = capture.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
-    stream.start_stream()
-
-    while True:
-
-        data = stream.read(4096, exception_on_overflow=False)
-
-        if len(data) == 0:
-            break
-
-        if recognizer.AcceptWaveform(data):
-            return recognizer.Result()
-
-
-def sayText(text):
-
-    engine = pyttsx3.init()
-    engine.setProperty('rate', 150)
-    engine.setProperty('volume',1.0)
-
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)
-    engine.say(text)
-    engine.runAndWait()
-    engine.stop()
-    return text
+eel.init('frontend')
+eel.start('index.html')
